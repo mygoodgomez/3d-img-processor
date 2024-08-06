2e43c276e146a7d549a93ccb72fd3e0c14d49825
Nik Gomez <niksterg11@gmail.com>
2024-08-06 16:59:08 -0700

    create PSDs instead of image exports

--- index.js
+++ index.js
@@ -1,8 +1,13 @@
-const fs = require('fs');
-var Jimp = require("jimp");
-
+import fs  from 'fs';
+import Jimp  from "jimp";
+import { Canvas, createCanvas, loadImage, ImageData } from 'canvas';
+import pkg from 'ag-psd';
+const { writePsdBuffer } = pkg;
+/*
+    DEFINE CONFIG VARS
+*/
 const SOURCE_DIR = '/Users/mygoodgomez/Downloads/samysphotolab_38108166_2024-08-06_1625/';
-const OUTPUT_DIR_PATH = SOURCE_DIR + 'output_imgs';
+const OUTPUT_PSD_DIR_PATH = SOURCE_DIR + 'output_psds';
 
 const RESIZE_WIDTH = 3840; // pixels for a 4k video
 const CROP_SIZE = {
@@ -15,18 +20,40 @@ const IMG_LOCS = {
 };
 
 
+/*
+    START SCRIPT
+*/
 
-if (!fs.existsSync(OUTPUT_DIR_PATH)){
-    fs.mkdirSync(OUTPUT_DIR_PATH);
+// create an output img & psd directories if it doesn't exist
+if (!fs.existsSync(OUTPUT_PSD_DIR_PATH)){
+    fs.mkdirSync(OUTPUT_PSD_DIR_PATH);
 }
 
+// go through all of the files in the source directory and split them up
+const imageGroupsForPS = [];
+const imageDataForPS = {};
+var totalNumImgsToProcess = 0;
+var groupIndex = 0;
 fs.readdirSync(SOURCE_DIR).forEach(fileName => {
     if (fileName.indexOf('tif') === -1) return;
+    totalNumImgsToProcess++;
+
+    const leftImgFileName = fileName.replace('.tif', '-left.jpg');
+    const rightImgFileName = fileName.replace('.tif', '-right.jpg');
+    
+    if(!imageGroupsForPS[groupIndex]) {
+        imageGroupsForPS[groupIndex] = [leftImgFileName, rightImgFileName];
+    } else {
+        imageGroupsForPS[groupIndex].push(leftImgFileName, rightImgFileName);
+
+        groupIndex++;
+    }
 
     const imgFullPath = SOURCE_DIR + fileName;
-    Jimp.read(imgFullPath, (err, photo) => {
+    Jimp.read(imgFullPath, async (err, photo) => {
         if (err) throw err;
     
+        const fileName = imgFullPath.replace(SOURCE_DIR, '');
         photo.resize(RESIZE_WIDTH, Jimp.AUTO);
     
         var leftImg = photo.clone();
@@ -38,9 +65,60 @@ fs.readdirSync(SOURCE_DIR).forEach(fileName => {
         leftImg.quality(90);
         rightImg.quality(90);
 
-        leftImg.write(OUTPUT_DIR_PATH + '/' + fileName.replace('.tif', '-1.jpg'));
-        rightImg.write(OUTPUT_DIR_PATH + '/' + fileName.replace('.tif', '-2.jpg'));
+        imageDataForPS[fileName.replace('.tif', '-left.jpg')] = leftImg;
+        imageDataForPS[fileName.replace('.tif', '-right.jpg')] = rightImg;
+        totalNumImgsToProcess--;
+
+        if(totalNumImgsToProcess === 0) {
+            await makeAllPhotoshopFiles();
+        }
     });        
 });
-    
 
+async function makeAllPhotoshopFiles() {
+    for(var i = 0; i < imageGroupsForPS.length; i++) {
+        try {
+            const groupToProcess = imageGroupsForPS[i];
+        
+            const img1 = new ImageData(Uint8ClampedArray.from(imageDataForPS[groupToProcess[0]].bitmap.data), CROP_SIZE.width, CROP_SIZE.height);
+            const canvas1 = createCanvas(CROP_SIZE.width, CROP_SIZE.height);
+            const ctx1 = canvas1.getContext('2d');
+            ctx1.putImageData(img1, 0, 0);
+    
+            const img2 = new ImageData(Uint8ClampedArray.from(imageDataForPS[groupToProcess[1]].bitmap.data), CROP_SIZE.width, CROP_SIZE.height);
+            const canvas2 = createCanvas(CROP_SIZE.width, CROP_SIZE.height);
+            const ctx2 = canvas2.getContext('2d');
+            ctx2.putImageData(img2, 0, 0);
+    
+            const img3 = new ImageData(Uint8ClampedArray.from(imageDataForPS[groupToProcess[2]].bitmap.data), CROP_SIZE.width, CROP_SIZE.height);
+            const canvas3 = createCanvas(CROP_SIZE.width, CROP_SIZE.height);
+            const ctx3 = canvas3.getContext('2d');
+            ctx3.putImageData(img3, 0, 0);
+    
+            const img4 = new ImageData(Uint8ClampedArray.from(imageDataForPS[groupToProcess[3]].bitmap.data), CROP_SIZE.width, CROP_SIZE.height);
+            const canvas4 = createCanvas(CROP_SIZE.width, CROP_SIZE.height);
+            const ctx4 = canvas4.getContext('2d');
+            ctx4.putImageData(img4, 0, 0);
+    
+            const children = [
+                { name: 'layer 1', canvas: canvas1 },
+                { name: 'layer 2', canvas: canvas2 },
+                { name: 'layer 3', canvas: canvas3 },
+                { name: 'layer 4', canvas: canvas4 }
+            ];
+              
+            const psd = { 
+                width: CROP_SIZE.width, 
+                height: CROP_SIZE.height,
+            };
+            const canvas = new Canvas(CROP_SIZE.width, CROP_SIZE.height);
+            const newPsd = { ...psd, children, canvas: canvas };
+            
+            const buffer = writePsdBuffer(newPsd);
+            fs.writeFileSync(OUTPUT_PSD_DIR_PATH + '/' + groupToProcess[0].replace('.jpg', '.psd'), buffer);    
+        } catch (e) {
+            console.log(e);
+            throw e;
+        }
+    }
+}
